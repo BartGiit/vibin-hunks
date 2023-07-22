51d4eb7adb6de27cf17b7dc13c5dbed49d854223
Bartek <mbartek01@gmail.com>
2023-07-22 13:30:55 +0200

    git commit src/components/SocialMedia/SocialMedia.tsx     git commit src/images/Amor.mov

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {FaDiscord,FaInstagram,FaTwitter} from 'react-icons/fa';
+import { FaDiscord, FaInstagram, FaTwitter } from 'react-icons/fa';
 import './SocialMedia.css';
 import desktopPleaseImage from '../../../src/images/text/desktopPlease.svg';
 import desktopDoItImage from '../../../src/images/text/desktopDoIt.svg';
@@ -8,50 +8,57 @@ import desktopYouKnowImage from '../../../src/images/text/desktopYouKnow.svg';
 import logo from '../../../src/images/logoNoShadow.svg';
 
 export const SocialMedia = ({ onHeightChange, Mobile }: { onHeightChange: (distance: number) => void; Mobile: boolean }) => {
-
+  
   useEffect(() => {
     const handleScroll = () => {
       const totalHeight = document.documentElement.scrollHeight;
-  
+
       onHeightChange(totalHeight);
     };
-  
+
     window.addEventListener('load', handleScroll);
     window.addEventListener('scroll', handleScroll);
-  
+
     return () => {
       window.removeEventListener('load', handleScroll);
       window.removeEventListener('scroll', handleScroll);
     };
-  },);
+  }, []);
 
-    return (
-      <div className={"SocialMedia"}>
-        <div className="TextWrapper">
-          <div className="Text">
-            <div className="Love">
-                <p>Show us your love!</p>
-            </div>
-            <div className="Icons">
-                <a href="toBeAdded" target="_blank"><FaTwitter className={"IconFontAwesome"}/></a>
-                <a href="toBeAdded" target="_blank"><FaInstagram className={"IconFontAwesome"}/></a>
-                <a href="toBeAdded" target="_blank"><FaDiscord className={"IconFontAwesome"}/></a>
-                <a href="toBeAdded" target="_blank"><img src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Transparent%20White.svg" alt="Vibin' Hunks Opensea" /></a>
-            </div>
-            <div className="Logo">
-              <img src={logo} alt="Vibin Hunks Collection Logo" />
-            </div>
-            <img className="textSVG" src={desktopPleaseImage} alt="Hunks Social Media NFT"/>
-            {Mobile ? (<img src={mobileDoItImage} className={"textSVGsecond"} alt="Hunks Social Media NFT" />) : (<img src={desktopDoItImage} className={"textSVGsecond"} alt="Hunks Social Media NFT" />)}
-          </div>
-        </div>
-        <div className="Image">
+  const videoSource = () => {
+    // Check if the browser is Safari
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    // Use different video source based on the browser
+    return isSafari ? require(`../../../src/images/Amor.hevc.mp4`) : require(`../../../src/images/Amor.webm`);
+  };
+
+  return (
+    <div className={"SocialMedia"}>
+      <div className="TextWrapper">
+        <div className="Text">
+          <div className="Love">
             <p>Show us your love!</p>
-            <img className="textSVGthird" src={desktopYouKnowImage} alt="Hunks Social Media NFT"/>
-            <video autoPlay loop muted>
-            <source src={require(`../../../src/images/Amor.mov`)} type="video/mp4" />
-            </video>
+          </div>
+          <div className="Icons">
+            <a href="toBeAdded" target="_blank"><FaTwitter className={"IconFontAwesome"} /></a>
+            <a href="toBeAdded" target="_blank"><FaInstagram className={"IconFontAwesome"} /></a>
+            <a href="toBeAdded" target="_blank"><FaDiscord className={"IconFontAwesome"} /></a>
+            <a href="toBeAdded" target="_blank"><img src="https://storage.googleapis.com/opensea-static/Logomark/Logomark-Transparent%20White.svg" alt="Vibin' Hunks Opensea" /></a>
+          </div>
+          <div className="Logo">
+            <img src={logo} alt="Vibin Hunks Collection Logo" />
+          </div>
+          <img className="textSVG" src={desktopPleaseImage} alt="Hunks Social Media NFT" />
+          {Mobile ? (<img src={mobileDoItImage} className={"textSVGsecond"} alt="Hunks Social Media NFT" />) : (<img src={desktopDoItImage} className={"textSVGsecond"} alt="Hunks Social Media NFT" />)}
         </div>
       </div>
-    );
-  }
+      <div className="Image">
+        <p>Show us your love!</p>
+        <img className="textSVGthird" src={desktopYouKnowImage} alt="Hunks Social Media NFT" />
+        <video autoPlay loop muted>
+          <source src={videoSource()} type={/^((?!chrome|android).)*safari/i.test(navigator.userAgent) ? 'video/mp4; codecs=hevc' : 'video/webm'} />
+        </video>
+      </div>
+    </div>
+  );
+};
