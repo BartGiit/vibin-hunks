946cf5b19a932afa40e045baff8e9dfe8cfbd6c5
Bartlomiej Michalowski <mbartek01@gmail.com>
2023-09-09 19:05:12 +0200

    git commit   src/components/PFPHunks/PFPHunks-Gallery.css git commit   src/components/PFPHunks/PFPHunks-Gallery.tsx git add    src/images/creators/Bartolini.jpeg git add   src/images/creators/Kamillo.jpeg git add   src/images/creators/Michael.jpeg git add   src/images/creators/Nicco.jpeg

--- src/components/PFPHunks/PFPHunks-Gallery.tsx
+++ src/components/PFPHunks/PFPHunks-Gallery.tsx
@@ -1,6 +1,17 @@
 import React, {useState} from 'react';
 import './PFPHunks-Gallery.css';
 
+import Image11 from '../../../src/images/PFPHunks/1-1-Vibin-Hunks-Profile.jpeg';
+import Image12 from '../../../src/images/PFPHunks/1-2-Vibin-Hunks-Profile.jpeg';
+import Image13 from '../../../src/images/PFPHunks/1-3-Vibin-Hunks-Profile.jpeg';
+import Image21 from '../../../src/images/PFPHunks/2-1-Vibin-Hunks-Profile.jpeg';
+import Image22 from '../../../src/images/PFPHunks/2-2-Vibin-Hunks-Profile.jpeg';
+import Image23 from '../../../src/images/PFPHunks/2-3-Vibin-Hunks-Profile.jpeg';
+import Image31 from '../../../src/images/PFPHunks/3-1-Vibin-Hunks-Profile.jpeg';
+import Image32 from '../../../src/images/PFPHunks/3-2-Vibin-Hunks-Profile.jpeg';
+import Image33 from '../../../src/images/PFPHunks/3-3-Vibin-Hunks-Profile.jpeg';
+
+
 export const Gallery = () => {
 
   const [index, setIndex] = useState(["2","1","3"]);
@@ -8,14 +19,17 @@ export const Gallery = () => {
 
   const changeImage = (e:string) => {
     let newValue:number;
-    if(e === "First")
-    {newValue = image.First + 1;
+    if(e === "First"){ 
+      newValue = image.First + 1;
+      console.log(newValue)
     }else if (e === "Second"){
       newValue = image.Second + 1;
+      console.log(newValue)
     }else{
       newValue = image.Third + 1;
+      console.log(newValue)
     }
-    if(newValue === 5){
+    if(newValue === 4){
       newValue = 1;
     }
     setImage(prevState => ({
@@ -50,15 +64,29 @@ export const Gallery = () => {
     }
   }
 
-  const handleClick = (e:any) => {
-    e.target.style.zIndex === "3" ? changeImage(e.target.className) : changeIndex(e.target.style.zIndex);
+  const handleClick = (e: any) => {
+    e.currentTarget.style.zIndex === "3" 
+      ? changeImage(e.currentTarget.className) 
+      : changeIndex(e.currentTarget.style.zIndex);
   }
 
     return (
       <div className={"Gallery"}>
-        <div className="First" style={{zIndex:index[0], backgroundImage: `url(${require("../../../src/images/PFPHunks/1-" + image.First + "-Vibin-Hunks-Profile.jpeg")})`}} onClick={handleClick}></div>
-        <div className="Second" style={{zIndex:index[1], backgroundImage: `url(${require("../../../src/images/PFPHunks/2-" + image.Second + "-Vibin-Hunks-Profile.jpeg")})`}} onClick={handleClick}></div>
-        <div className="Third" style={{zIndex:index[2], backgroundImage: `url(${require("../../../src/images/PFPHunks/3-" + image.Third + "-Vibin-Hunks-Profile.jpeg")})`}} onClick={handleClick}></div>
+        <div className="First" style={{zIndex:index[0]}} onClick={handleClick}>
+          <img src={Image11} alt="Hunk PFP" style={{display: image.First === 1 ? "block" : "none"}} />
+          <img src={Image12} alt="Hunk PFP" style={{display: image.First === 2 ? "block" : "none"}} />
+          <img src={Image13} alt="Hunk PFP" style={{display: image.First === 3 ? "block" : "none"}} />
+        </div>
+        <div className="Second" style={{zIndex:index[1]}} onClick={handleClick}>
+          <img src={Image21} alt="Hunk PFP" style={{display: image.Second === 1 ? "block" : "none"}} />
+          <img src={Image22} alt="Hunk PFP" style={{display: image.Second === 2 ? "block" : "none"}} />
+          <img src={Image23} alt="Hunk PFP" style={{display: image.Second === 3 ? "block" : "none"}} />
+        </div>
+        <div className="Third" style={{zIndex:index[2]}} onClick={handleClick}>
+          <img src={Image31} alt="Hunk PFP" style={{display: image.Third === 1 ? "block" : "none"}} />
+          <img src={Image32} alt="Hunk PFP" style={{display: image.Third === 2 ? "block" : "none"}} />
+          <img src={Image33} alt="Hunk PFP" style={{display: image.Third === 3 ? "block" : "none"}} />
+        </div>
       </div>
     );
   }
