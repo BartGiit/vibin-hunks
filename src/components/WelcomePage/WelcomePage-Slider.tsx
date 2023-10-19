b2029e18030e80cceef61967fd1765b69bfc0100
Bartlomiej Michalowski <mbartek01@gmail.com>
2023-10-19 12:58:34 +0200

    git commit   src/components/IncognitoHunks/IncognitoHunks.css git commit   src/components/WelcomePage/WelcomePage-NavBar.css git commit   src/components/WelcomePage/WelcomePage-Slider.css git commit   src/components/WelcomePage/WelcomePage-Slider.tsx git delete    src/images/IncognitoHunks/cs.png git commit   src/images/IncognitoHunks/cs.svg git delete    src/images/IncognitoHunks/question.png git commit   src/images/IncognitoHunks/question.svg git delete    src/images/IncognitoHunks/question_small.png git commit   src/images/IncognitoHunks/question_small.svg git delete    src/images/IncognitoHunks/tapes.png git commit   src/images/IncognitoHunks/tapes.svg git commit   src/images/IncognitoHunks/tapes_small.png git commit   src/images/logo.png git delete    src/images/logo.svg

--- src/components/WelcomePage/WelcomePage-Slider.tsx
+++ src/components/WelcomePage/WelcomePage-Slider.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, SetStateAction, useState, useEffect} from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Navigation, Autoplay} from 'swiper';
-import logo from '../../../src/images/logo.svg';
+import logo from '../../../src/images/logo.png';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './WelcomePage-Slider.css';
